f0eea102659cdcbdfdefe171566ced8a118baa97
Oliver <oliver.nic012@gmail.com>
2024-04-09 21:27:31 -0500

    Adjust controller multiplier

--- player.gd
+++ player.gd
@@ -3,15 +3,12 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 @export var LOOK_SENSITIVITY: float
-const CONTROLLER_LOOK_MULTIPLIER: int = 25
+const CONTROLLER_LOOK_MULTIPLIER: int = 35
 const TILT_LOWER_LIMIT: float = -89
 const TILT_UPPER_LIMIT: float = 89
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
 var camera: Camera3D
-
 var mouseDelta: Vector2 = Vector2()
 
 func _ready():

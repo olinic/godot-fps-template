d2dc333c93b44eafcee2805eeb37b29b6c3d99b6
Oliver <oliver.nic012@gmail.com>
2024-04-20 23:28:03 -0500

    Add speed function

--- player.gd
+++ player.gd
@@ -29,16 +29,19 @@ func _physics_process(delta):
 func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-	
+
+func getSpeed():
+	return SPEED
+
 func move(delta):
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * getSpeed()
+		velocity.z = direction.z * getSpeed()
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, getSpeed())
+		velocity.z = move_toward(velocity.z, 0, getSpeed())
 	move_and_slide()
 
 func aim(delta):

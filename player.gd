f79c0305e0df3d5499cdf18199734d17855d504f
Oliver <oliver.nic012@gmail.com>
2024-04-20 23:32:26 -0500

    Fix name

--- player.gd
+++ player.gd
@@ -30,18 +30,18 @@ func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-func getSpeed():
+func get_speed():
 	return SPEED
 
 func move(delta):
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * getSpeed()
-		velocity.z = direction.z * getSpeed()
+		velocity.x = direction.x * get_speed()
+		velocity.z = direction.z * get_speed()
 	else:
-		velocity.x = move_toward(velocity.x, 0, getSpeed())
-		velocity.z = move_toward(velocity.z, 0, getSpeed())
+		velocity.x = move_toward(velocity.x, 0, get_speed())
+		velocity.z = move_toward(velocity.z, 0, get_speed())
 	move_and_slide()
 
 func aim(delta):

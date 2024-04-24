d1c0028a7a894cc6a8fa71f607da1b0498599132
Oliver <oliver.nic012@gmail.com>
2024-04-23 22:13:13 -0500

    Camera improvements based on tutorial

---
+++ Player/SmoothCamera.gd
@@ -0,0 +1,16 @@
+extends Camera3D
+
+@export var speed := 44.0
+
+func _physics_process(delta: float) -> void:
+	smooth_aiming(delta)
+	keep_parent_position()
+
+func smooth_aiming(delta:float) -> void:
+	var weight = clampf(delta * speed, 0.0, 1.0)
+	global_transform = global_transform.interpolate_with(
+		get_parent().global_transform, weight
+	)
+	
+func keep_parent_position() -> void:
+	global_position = get_parent().global_position
--- Player/player.gd
+++ Player/player.gd
@@ -5,30 +5,30 @@ const SPEED: float = 5.0
 const JUMP_VELOCITY: float = 4.5
 @export var HORIZONTAL_LOOK_SENSITIVITY: float
 @export var VERTICAL_LOOK_SENSITIVITY: float
-const CONTROLLER_LOOK_MULTIPLIER: int = 50
-const VERTICAL_LOOK_LOWER_LIMIT: float = -89
-const VERTICAL_LOOK_UPPER_LIMIT: float = 89
+const CONTROLLER_LOOK_MULTIPLIER: float = 0.08
+const VERTICAL_LOOK_LOWER_LIMIT: float = -90
+const VERTICAL_LOOK_UPPER_LIMIT: float = 90
 const SPRINT_LIMIT_ANGLE_MULTIPLIER: float = 0.22 # 0.25 = 45 degrees
 const SPRINT_LIMIT_ANGLE_LEFT: float = -PI * SPRINT_LIMIT_ANGLE_MULTIPLIER
 const SPRINT_LIMIT_ANGLE_RIGHT: float = -PI * (1 - SPRINT_LIMIT_ANGLE_MULTIPLIER)
 var is_sprinting: bool = false
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-var camera: Camera3D
-var mouseDelta: Vector2 = Vector2()
+@onready var camera_controller = $CameraController
+
+var mouse_motion: Vector2 = Vector2()
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	camera = get_node("CameraController/Camera3D")
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		mouseDelta = event.relative
+		mouse_motion = -event.relative * 0.001
 
 func _physics_process(delta):
+	aim(delta)
 	apply_gravity(delta)
 	move(delta)
-	aim(delta)
 	handle_jump()
 
 func apply_gravity(delta):
@@ -69,24 +69,27 @@ func get_speed():
 		return SPEED
 
 func aim(delta):
-	if mouseDelta != Vector2.ZERO:
+	if mouse_motion != Vector2.ZERO:
 		mouse_look(delta)
 	else:
 		controller_look(delta)
 	
 func mouse_look(delta):
-	adjust_camera_look(delta, mouseDelta)
-	mouseDelta = Vector2()
+	adjust_camera_look(delta, mouse_motion)
+	mouse_motion = Vector2()
 
 func controller_look(delta):
 	var aim_dir = Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down")
-	adjust_camera_look(delta, aim_dir * CONTROLLER_LOOK_MULTIPLIER)
+	adjust_camera_look(delta, -aim_dir * CONTROLLER_LOOK_MULTIPLIER)
 
 func adjust_camera_look(delta, look_rotation: Vector2):
-	look_rotation *= delta
-	camera.rotation_degrees.x -= look_rotation.y * VERTICAL_LOOK_SENSITIVITY
-	camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, VERTICAL_LOOK_LOWER_LIMIT, VERTICAL_LOOK_UPPER_LIMIT)
-	rotation_degrees.y -= look_rotation.x * HORIZONTAL_LOOK_SENSITIVITY
+	rotate_y(look_rotation.x)
+	camera_controller.rotate_x(look_rotation.y)
+	camera_controller.rotation_degrees.x = clampf(
+		camera_controller.rotation_degrees.x, 
+		VERTICAL_LOOK_LOWER_LIMIT, 
+		VERTICAL_LOOK_UPPER_LIMIT
+	)
 
 func handle_jump():
 	if Input.is_action_just_pressed("jump") and is_on_floor():

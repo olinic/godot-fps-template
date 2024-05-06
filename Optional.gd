ad0cff96030872641ec342d91ad0b250557f9888
Oliver <oliver.nic012@gmail.com>
2024-05-05 22:04:01 -0500

    Add optional

---
+++ Optional.gd
@@ -0,0 +1,30 @@
+class_name Optional
+extends RefCounted
+
+var _value: Variant
+var _is_present: bool = false
+
+static func of(value: Variant) -> Optional:
+	var opt = Optional.new()
+	opt._value = value
+	opt._is_present = true
+	return opt
+
+static func empty() -> Optional:
+	var opt = Optional.new()
+	opt._is_present = false
+	return opt
+
+func get_value() -> Variant:
+	assert(_is_present, "Optional is empty")
+	return _value
+
+func or_else(other: Variant) -> Variant:
+	return _value if _is_present else other
+
+func is_present() -> bool:
+	return _is_present
+
+func if_present(callback) -> void:
+	if _is_present:
+		callback.call(_value)

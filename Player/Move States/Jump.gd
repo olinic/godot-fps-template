2a0a98575255f33dbcb75cba886759d6fa358b04
retsofoster <elijah.foster@live.com>
2024-05-08 16:57:33 -0400

    Added advanced jumping
    Now based on jump height in order to math out velocity/

--- Player/Move States/Jump.gd
+++ Player/Move States/Jump.gd
@@ -1,7 +1,6 @@
 class_name Jump
 
 const SPEED: float = 300.0
-const JUMP_VELOCITY: float = 4.5
 
 var _player: Player
 var _player_is_on_floor: Callable
@@ -9,6 +8,9 @@ var _aerial_dir: Vector3 = Vector3.ZERO
 var _speed: float
 var _target_state: Variant
 var _next_state := Optional.empty()
+var _gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+@export var jump_height: float = 1.0
 
 func _init(player: Player, player_is_on_floor: Callable, aerial_dir: Vector3, speed: float, target_state) -> void:
 	self._player = player
@@ -16,12 +18,12 @@ func _init(player: Player, player_is_on_floor: Callable, aerial_dir: Vector3, sp
 	self._aerial_dir = aerial_dir
 	self._speed = speed
 	self._target_state = target_state
-	
-	player.velocity.y = JUMP_VELOCITY
+
+	player.velocity.y = sqrt(jump_height * 2.0 * _gravity)
 	print("Entered Jump State.")
 
 func get_initial_velocity_change() -> Vector3:
-	return Vector3(0, JUMP_VELOCITY, 0)
+	return Vector3(0, jump_height, 0)
 
 func get_velocity(_delta: float, _input_dir: Vector2) -> Vector3:
 	return _aerial_dir

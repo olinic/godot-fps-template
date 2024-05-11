02b12c7da1e92bd8d6292edb49b1b219cc9567b6
retsofoster <elijah.foster@live.com>
2024-05-11 11:28:07 -0400

    Improved level Scene to include slopes and "buildings"

--- Player/Move States/Jump.gd
+++ Player/Move States/Jump.gd
@@ -1,7 +1,6 @@
 class_name Jump
 
 const SPEED: float = 300.0
-const FALL_MULTIPLIER: float = 2.0
 
 var _player: Player
 var _player_is_on_floor: Callable
@@ -13,6 +12,7 @@ var _gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var _state_name = "Jump"
 
 @export var jump_height: float = 1.0
+@export var fall_multiplier: float = 2.0
 
 func _init(player: Player, player_is_on_floor: Callable, aerial_dir: Vector3, speed: float, target_state) -> void:
 	self._player = player
@@ -31,7 +31,7 @@ func get_velocity(delta: float, _input_dir: Vector2) -> Vector3:
 	if _aerial_dir.y >= 0:
 		_aerial_dir.y -= _gravity * delta
 	else:
-		_aerial_dir.y -= _gravity * delta * FALL_MULTIPLIER
+		_aerial_dir.y -= _gravity * delta * fall_multiplier
 	return _aerial_dir
 
 func get_next_state(_input_dir: Vector2) -> Optional:

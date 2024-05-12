62597fa08c2c1b5ad7058a24917f3da1f55ce63d
retsofoster <elijah.foster@live.com>
2024-05-12 12:42:39 -0400

    added recoil and raycast to hitscan weapon

--- Weapons/hitscan_weapon.gd
+++ Weapons/hitscan_weapon.gd
@@ -1,12 +1,21 @@
 extends Node3D
 
 @export var fire_rate := 14.0
+@export var recoil := 0.05
+@export var weapon_mesh: Node3D
 
 @onready var cooldown_timer: Timer = $CooldownTimer
+@onready var weapon_position: Vector3 = weapon_mesh.position
+@onready var ray_cast_3d: RayCast3D = $RayCast3D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("fire"):
 		if cooldown_timer.is_stopped():
-			cooldown_timer.start(1.0 / fire_rate)
-			print("weapon fired!")
+			shoot()
+	weapon_mesh.position = weapon_mesh.position.lerp(weapon_position, delta * 10.0)
+func shoot() -> void: 
+	cooldown_timer.start(1.0 / fire_rate)
+	printt("weapon fired!", ray_cast_3d.get_collider())
+	weapon_mesh.position.z += recoil
+	

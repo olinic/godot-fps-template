4a219bf519d7b9c1b03bce34abffd21203cf1544
Oliver <oliver.nic012@gmail.com>
2024-05-11 21:55:05 -0500

    Clean up

--- Player/Move States/Sprint.gd
+++ Player/Move States/Sprint.gd
@@ -46,23 +46,3 @@ func get_next_state(input_dir: Vector2) -> Optional:
 			or !Sprint.is_moving_forward(input_dir)):
 		_next_state = Optional.of(Walk.new(_player, _player_is_on_floor))
 	return _next_state
-
-func process(delta):
-	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	
-	if (Input.is_action_just_released("keyboard_sprint") 
-			or !Sprint.is_moving_forward(input_dir)):
-		_player.change_move_state(Walk.new(_player, _player_is_on_floor))
-		
-	var direction = (_player.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	handle_jump(direction)
-	_player.move(direction, SPRINT_SPEED, delta)
-	
-func handle_jump(direction):
-	if Input.is_action_just_pressed("jump"):
-		_player.change_move_state(
-				Jump.new(_player, _player_is_on_floor, direction, SPRINT_SPEED, Sprint.new(_player, func(): return)))
-	elif !_player.is_on_floor():
-		_player.change_move_state(
-				Fall.new(_player, _player_is_on_floor, direction, SPRINT_SPEED, Sprint.new(_player, func(): return)))
-

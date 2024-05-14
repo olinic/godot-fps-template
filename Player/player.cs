8f61229b0f3fd9ff93cffad9941dbedfe81411b0
Oliver <oliver.nic012@gmail.com>
2024-05-13 23:03:28 -0500

    Remove gdunit for now. Setup initial dotnet player script. Attempt to get movement working.

---
+++ Player/player.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public partial class player : CharacterBody3D
+{
+    public override void _Ready()
+    {
+
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_backward");
+        Vector3 direction = new Vector3(inputDir.X, 0, inputDir.Y).Normalized();
+        float speed = 300;
+        Velocity = new Vector3(0, 0, 0);
+        if (direction.IsZeroApprox())
+        {
+            Velocity = new Vector3(
+                    direction.X * speed * (float)delta,
+                    0,
+                    direction.Z * speed * (float)delta);
+
+        }
+        this.MoveAndSlide();
+    }
+}

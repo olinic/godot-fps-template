a2fff29e96d7de3749a35b32b8dcb77e11bb440c
retsofoster <elijah.foster@live.com>
2024-05-27 21:37:53 -0400

    Implemented Jump Test C#

--- test/Player/Move States/JumpTest.cs
+++ test/Player/Move States/JumpTest.cs
@@ -29,7 +29,6 @@ public class Jumptest{
     [TestCase]
      public void GetInitialVelocityChange()
     {
-        //(sprint_state.get_initial_velocity_change()).is_equal(Vector3.ZERO)
         IMoveState JumpState = (IMoveState) Jump;
         AssertVector(JumpState.GetInitialVelocityChange())
             .Equals(new Vector3(0, (float) Math.Sqrt(Jump.JumpHeight/Jump.Gravity * 2.0f) *  Jump.Gravity,0));
@@ -38,10 +37,27 @@ public class Jumptest{
     [TestCase]
     public void GetVelocity()
     {
-        Jump.GetVelocity(1.0f, new Vector2(0.5f, 0.5f), new Basis());
-        /* AssertFloat().Equals(1);
-        AssertFloat().IsLess(0);
-        AssertFloat().Equals(1); */
+        var _velocity = Jump.GetVelocity(1.0f, new Vector2(0.5f, 0.5f), new Basis());
+        AssertFloat(_velocity.X).Equals(1);
+        AssertFloat(_velocity.Y).IsLess(0); //falling
+        AssertFloat(_velocity.Z).Equals(1); 
     }
     
+   [TestCase( 1, TestName= "Walk")]
+   [TestCase( 2, TestName= "Sprint")]
+    public void GivenLanding_GetNextState_ReturnsWalkOrSprint(int x)
+    {
+        if(x == 1)
+        {
+            Jump.SetTargetState(new Walk(Player));
+            AssertObject(Jump.GetNextState(Vector2.Up, true).GetValue()).IsInstanceOf<Walk>();        
+        }
+        else if(x == 2)
+        {
+            Jump.SetTargetState(new Sprint(Player));
+            AssertObject(Jump.GetNextState(Vector2.Up, true).GetValue()).IsInstanceOf<Sprint>();  
+        }
+    }
+    
+    //TODO: test no transition to any state when landed is false.
 }

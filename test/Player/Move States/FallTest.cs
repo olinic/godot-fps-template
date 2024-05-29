4ce32c2d2c94e77f9d879adaac88b2fca02d4edc
retsofoster <elijah.foster@live.com>
2024-05-28 23:48:39 -0400

    Cleaned Fall Test

--- test/Player/Move States/FallTest.cs
+++ test/Player/Move States/FallTest.cs
@@ -4,12 +4,14 @@ using static GdUnit4.Assertions;
 
 
 [TestSuite]
-public class FallTest{
+public class FallTest
+{
     private player Player;
     private Fall Fall;
 
     [Before]
-    public void Before(){
+    public void Before()
+    {
         Player = new player();
     }
 
@@ -27,7 +29,6 @@ public class FallTest{
       [TestCase]
     public void GetInitialVelocityChange()
     {
-        //(sprint_state.get_initial_velocity_change()).is_equal(Vector3.ZERO)
         IMoveState FallState = (IMoveState) Fall;
         AssertVector(FallState.GetInitialVelocityChange())
             .Equals(Vector3.Zero);

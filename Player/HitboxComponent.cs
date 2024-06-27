ce84920cb2f8fe497019edf07c1a954869cf1101
retsofoster <elijah.foster@live.com>
2024-06-26 23:49:34 -0400

    removed "Entered Area" (GD.Print)

--- Player/HitboxComponent.cs
+++ Player/HitboxComponent.cs
@@ -7,7 +7,7 @@ public partial class HitboxComponent : Node3D
 
 	public void OnHitboxComponentBodyEntered(Node3D body)
 	{
-		GD.Print("Entered Area");
+		
 		if (body is ICanAttack attacker)
 		{
 			health.ApplyDamage(attacker.GetAttack());

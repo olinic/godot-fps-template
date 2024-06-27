39365177496cfbd8e4bc0fa273ab1f3c3f3157e7
retsofoster <elijah.foster@live.com>
2024-06-27 01:35:40 -0400

    Translated hitscan weapon to C#

---
+++ Enemy/enemy.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class enemy : CharacterBody3D
+{
+	
+	
+
+	
+	
+
+	
+}
---
+++ Weapons/hitscan_weapon.cs
@@ -0,0 +1,47 @@
+using Godot;
+using System;
+
+public partial class hitscan_weapon : Node3D
+{
+	[Export]
+    private float _FireRate = 14.0f;
+    [Export]
+    private float _Recoil = 0.05f;
+    [Export]
+    private Node3D _WeaponMesh;
+
+    private Timer _CooldownTimer;
+    private Vector3 _WeaponPosition;
+    private RayCast3D _RayCast3D;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		 _CooldownTimer = GetNode<Timer>("CooldownTimer");
+        _WeaponPosition = _WeaponMesh.Position;
+        _RayCast3D = GetNode<RayCast3D>("RayCast3D");
+
+        _RayCast3D.CollideWithAreas = true;
+        _RayCast3D.CollideWithBodies = false;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		 if(Input.IsActionPressed("fire"))
+        {
+            if(_CooldownTimer.IsStopped())
+            {
+                Shoot();
+            }
+        }
+       
+        _WeaponMesh.Position = _WeaponMesh.Position.Lerp(_WeaponPosition, (float)delta * 10.0f);
+	}
+	 private void Shoot()
+    {
+        _CooldownTimer.Start(1.0f / _FireRate);
+        GD.PrintT("Weapon Fired!", _RayCast3D.GetCollider());
+        _WeaponMesh.Position = _WeaponMesh.Position with { Z = _WeaponMesh.Position.Z + _Recoil};
+    }
+}

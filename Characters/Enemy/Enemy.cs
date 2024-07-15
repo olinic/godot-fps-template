893909843ded53fc57c8653e91ce76a5f32ab395
Oliver <oliver.nic012@gmail.com>
2024-07-14 22:52:52 -0500

    Comment out shoot code for now. Wire up enemy properties.

--- Characters/Enemy/Enemy.cs
+++ Characters/Enemy/Enemy.cs
@@ -8,16 +8,16 @@ namespace FPS.Characters.Enemy;
 
 public partial class Enemy : CharacterBody3D
 {
-    [Export] WeaponHandler weaponHandler;
+    [Export] private WeaponHandler _weaponHandler;
     [Export] private Timer _CooldownTimer;
     public override void  _PhysicsProcess(double delta)
     {
-        if(_CooldownTimer.IsStopped())
-        {
-            GD.Print("enemy shot!");
-            weaponHandler.Shoot(delta);
-            _CooldownTimer.Start();
-        }
+        // if(_CooldownTimer.IsStopped())
+        // {
+        //     GD.Print("enemy shot!");
+        //     weaponHandler.Shoot(delta);
+        //     _CooldownTimer.Start();
+        // }
     }
     public void OnHealthComponentHealthDepleted()
     {   

bb814380ec6cce27d63eeda60dd68e42567ea640
Oliver <oliver.nic012@gmail.com>
2024-07-15 23:23:19 -0500

    Setup hitscan weapon test.

--- Weapons/HitscanWeapon.cs
+++ Weapons/HitscanWeapon.cs
@@ -6,46 +6,45 @@ public partial class HitscanWeapon : Node3D, IGun, ICanAttack
 {
 	[Export] public float FireRate;
     [Export] public float _Recoil;
-    [Export] private Node3D _WeaponMesh;
+    [Export] public Node3D WeaponMesh;
    
     [Export] public int AmmoCapacity;
     [Export] private int _Damage;
     [Export] public FireMode FireMode;
     [Export] public WeaponType WeaponType;
+    [Export] public RayCast3D RayCast;
 
     public int CurrentAmmo;
     private Vector3 _WeaponPosition;
-    private RayCast3D _RayCast3D;
 
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		CurrentAmmo = AmmoCapacity;
-        _WeaponPosition = _WeaponMesh.Position;
-        _RayCast3D = GetNode<RayCast3D>("RayCast3D");
+        _WeaponPosition = WeaponMesh.Position;
 
-        _RayCast3D.CollideWithAreas = true;
-        _RayCast3D.CollideWithBodies = false;
+        RayCast.CollideWithAreas = true;
+        RayCast.CollideWithBodies = false;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-        _WeaponMesh.Position = _WeaponMesh.Position.Lerp(_WeaponPosition, (float)delta * 10.0f);  
+        WeaponMesh.Position = WeaponMesh.Position.Lerp(_WeaponPosition, (float)delta * 10.0f);  
 	}
 
 	public void Shoot(double delta)
     {
         
         CurrentAmmo--;
-        if(_RayCast3D.GetCollider() is ICanTakeDamage target)
+        if(RayCast.GetCollider() is ICanTakeDamage target)
         {
-            GD.PrintT("Weapon Fired!", _RayCast3D.GetCollider());
+            GD.PrintT("Weapon Fired!", RayCast.GetCollider());
             
             target.ApplyDamage(GetAttack());
         }
-        _WeaponMesh.Position = _WeaponMesh.Position with { Z = _WeaponMesh.Position.Z + _Recoil};
+        WeaponMesh.Position = WeaponMesh.Position with { Z = WeaponMesh.Position.Z + _Recoil};
         
     }
     public void Reload()
--- test/Weapons/HitscanWeaponTest.cs
+++ test/Weapons/HitscanWeaponTest.cs
@@ -6,15 +6,30 @@ namespace FPS.Weapons;
 [TestSuite]
 public class HitscanWeaponTest
 {
+    private HitscanWeapon weapon;
+
+    [Before]
+    public void Before()
+    {
+        weapon = new()
+        {
+            WeaponMesh = new Node3D(),
+            RayCast = new RayCast3D(),
+            AmmoCapacity = 20
+        };
+        weapon._Ready();
+    }
+
     [TestCase]
     public void GivenFullCapacity_GetAmmoCount_ReturnsCount()
     {
-        
+        AssertInt(weapon.GetCurrentAmmo()).IsEqual(weapon.AmmoCapacity);
     }
 
     [TestCase]
     public void GivenShot_GetAmmoCount_ReturnsReducedCount()
     {
-        
+        weapon.Shoot(1);
+        AssertInt(weapon.GetCurrentAmmo()).IsEqual(weapon.AmmoCapacity - 1);
     }
 }

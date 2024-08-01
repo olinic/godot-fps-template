6602088639da05cbc12e9959526380a46f40218f
Oliver <oliver.nic012@gmail.com>
2024-07-31 21:32:30 -0500

    Refactor health component

--- Characters/Health/HealthComponent.cs
+++ Characters/Health/HealthComponent.cs
@@ -3,33 +3,43 @@ using Godot;
 namespace FPS.Characters.Health;
 public partial class HealthComponent : Node3D, ICanTakeDamage
 {
-	public Timer _RegenTimer;
-	//[Export] private float MAX_HEALTH = 1000;
-	public float MAX_HEALTH { get; init; } = 1000;
-
-	[Export] private float REGEN_DURATION;
-	private float REGEN_RATE;
-	
-	[Signal]
-	public delegate void health_depletedEventHandler();
-
-	[Signal]
-	public delegate void health_changedEventHandler(Health health);
-
+	public Timer _regenTimer;
+	public float MaxHealth { get; set; } = 1000;
+	public float RegenDurationSeconds { 
+		get => _regenDurationSeconds; 
+		set
+		{
+			_regenDurationSeconds = value;
+			SetRegenRate();
+		}
+	}
 	public Health Health { get; private set; }
+
+	[Signal] public delegate void health_depletedEventHandler();
+	[Signal] public delegate void health_changedEventHandler(Health health);
+
+	private float _regenDurationSeconds = 2;
+	private float _regenRate;
+	
+
+	public HealthComponent()
+	{
+		SetRegenRate();
+	}
+
+	private void SetRegenRate()
+	{
+		_regenRate = MaxHealth/RegenDurationSeconds;
+	}
 
 	public override void _Ready()
 	{
-		_RegenTimer = new() { OneShot = true, Autostart = false, WaitTime = 2.0f};
-		AddChild(_RegenTimer);
-		//duration is 2 sec
-		//health is 600
-		//rate 10
-		REGEN_RATE = MAX_HEALTH/REGEN_DURATION;
+		_regenTimer = new() { OneShot = true, Autostart = false, WaitTime = 2.0f};
+		AddChild(_regenTimer);
 		Health = new Health()
 		{
-			Max = MAX_HEALTH,
-			Value = MAX_HEALTH
+			Max = MaxHealth,
+			Value = MaxHealth
 		};
 		EmitSignal(SignalName.health_changed, Health);
 	}
@@ -39,21 +49,29 @@ public partial class HealthComponent : Node3D, ICanTakeDamage
 		ApplyHealthRegen(delta);
     }
 
+	private void ApplyHealthRegen(double delta)
+	{
+		if(_regenTimer.IsStopped())
+		{
+			AdjustHealth(Health.Value + _regenRate * (float) delta);
+		}
+	}
+
     public void ApplyDamage(Attack attack)
 	{
-		_RegenTimer.Start();
+		_regenTimer.Start();
 		AdjustHealth(Health.Value - attack.Damage);
 	}
 
 	private void AdjustHealth(float value)
 	{
-		float clamped = Mathf.Clamp(value, 0, MAX_HEALTH);
+		float clamped = Mathf.Clamp(value, 0, MaxHealth);
 
 		if(clamped != Health.Value)
 		{
 			Health = new Health()
 			{
-				Max = MAX_HEALTH,
+				Max = MaxHealth,
 				Value = clamped
 			};
 			EmitSignal(SignalName.health_changed, Health);
@@ -62,13 +80,5 @@ public partial class HealthComponent : Node3D, ICanTakeDamage
 				EmitSignal(SignalName.health_depleted);
 			}
 		}	
-	}
-
-	private void ApplyHealthRegen(double delta)
-	{
-		if(_RegenTimer.IsStopped())
-		{
-			AdjustHealth(Health.Value + REGEN_RATE * (float) delta);
-		}
 	}
 }
